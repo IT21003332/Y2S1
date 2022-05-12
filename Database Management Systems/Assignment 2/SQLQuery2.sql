1485733c04a78852391e5b7a63a2719e4247ed6e
Devindu Samarasinghe <it21003332@my.sliit.lk>
2022-05-12 12:23:26 +0530

    DMS content

--- Database Management Systems/Assignment 2/SQLQuery2.sql
+++ Database Management Systems/Assignment 2/SQLQuery2.sql
@@ -57,6 +57,7 @@ constraint empNo_fk foreign key(empNo) references lecturer(empNo)
 --Insertions 
 
 insert into lecturer values(1001,'Devindu Samarasinge','IT')
+insert into lecturer values(1002, 'Piyumath Siriwardane','SE')
 
 
 insert into student values(2001,'Pinky','cmb',0767676,'IT',3,3.4)
@@ -68,6 +69,7 @@ insert into student values(2005, 'Dilruk','kuru',027372,'CSE',3,3.6)
 insert into student values(2006, 'Deshan','kuru',027372,'IT',3,3.6)
 insert into student values(2007, 'Sheni','kuru',027372,'SE',3,3.6)
 insert into student values(2008, 'Piyama','kuru',027372,'ISE',3,3.9)
+insert into student values(2009, 'Ashan','cmb',0787878,'ISE',3,3.78)
 
 insert into project values('P002','Project 2',3,'SE Theory',2005,'3002')
 insert into project values('P001','Project 1',3,'IoT',2001,'3001')
@@ -81,6 +83,9 @@ insert into studentGroup values('3002',2005)
 insert into studentGroup values('3002',2006)
 insert into studentGroup values('3002',2007)
 insert into studentGroup values('3002',2008)
+
+insert into supervise values('P001',1001)
+insert into supervise values('P002',1002)
 
 drop table studentGroup
 
@@ -96,3 +101,85 @@ select sg.groupNo, count(s.studentName)
 from student s, studentGroup sg
 where s.studentNo = sg.studentNo
 group by sg.groupNo
+
+-- find the average of the GOA of the groups 
+
+	
+
+-- find the students who are under the leaders of each project 
+
+
+--find the student name and student ID of students who are not assigned with a group 
+
+
+
+select s.studentName, s.studentNo
+from student s
+where s.studentNo not in (select sg.studentNo
+							from studentGroup sg
+							where s.studentNo = sg.studentNo)
+
+-- find the total numnber of students in each group 
+
+
+select sg.groupNo, count(s.studentNo)
+from student s left outer join studentgroup sg on s.studentNo = sg.studentNo
+group by sg.groupNo
+
+
+--create a procedure that would return the Leader name and ID when given the parameter of projectNo
+
+create procedure leaderDetails(@projectNo varchar(10), @leadName varchar(30) output,@leadId int output)
+as 
+begin
+
+	select @leadName = s.studentName, @leadId = s.studentNo
+			from student s, project p 
+			where s.studentNo = p.leader and p.projectNo = @projectNo
+end 
+
+declare @leaderName varchar(30), @leaderId int
+exec leaderDetails 'P001',@leaderName output, @leaderId output
+
+print concat('Leader Name : ',@leaderName)
+print concat('Leader ID: ',@leaderId)
+
+
+
+--create a trigger when updating or inserting project Details. will update another foreign key table 
+
+create trigger addLogEntry		--what we are trying to do is, when the account table is updated, the foreign key constraints are captured and it automatically updates the accountLog
+on account --WE USE FOR - WHICH WILL UPDATE THE BASE TABLE TOO
+for insert, update
+as 
+begin
+
+declare @accountNo int
+declare @balance real 
+
+select @accountNo = accountNo, @balance = balance from inserted
+insert into accountlog(accountNo,balance,date) values(@accountNo,@balance,getdate())
+end					
+
+
+
+create trigger studentGroupCheck
+on studentGroup
+for insert,update
+as
+begin 
+
+declare @studentNo int 
+declare @groupcount int
+
+select @studentNo = studentNo from inserted
+select @groupcount = count(GroupNo) from studentGroup where studentNo = @studentNo
+
+if @groupcount > 1
+	begin 
+		print 'Student can only be in one single group'
+		rollback transaction 
+	end 
+end 
+
+insert into studentGroup values('3001',2008)
---
+++ Database Management Systems/Week 4/IN outer right stuff.sql
@@ -0,0 +1,22 @@
+-- INNER JOIN Condition 
+
+-- inner join returns the matching rows and does not return the unmatching rows. Same casual matching concept is INNER JOIN 
+
+-- LEFT OUTER JOIN 
+
+-- Returns all the rows from the left ( irregardless of it matching with anyone, it returns everything from left) and returns all the matching rows from right 
+
+-- RIGHT OUTER JOIN 
+
+-- Same case as last time except it returns all the right hand side rows and only matching ones from the left 
+
+
+--EXISTS 
+
+-- returns any row in subquery 
+-- whatever you can write in IN, you can write in Exists || Whatever you can write in NOT IN can be written in NOT EXIST too 
+
+-- This returns true or false depending of the subquery any row. and if that condition is successful outer query will work 
+
+
+
